5626578f26b27b0fcec75e9f9170f4ef422c5fd8
withnolight <121738028+withnolight@users.noreply.github.com>
2025-05-29 08:34:43 +0800

    code update

---
+++ 01_2023141490258_刘润泽/n_queens.cpp
@@ -0,0 +1,53 @@
+#include<cstdio>
+#include<cmath>
+int location[21];
+int number;
+int count = 0;
+using namespace std;
+
+bool is_safe(int x,int y);
+int print();
+
+void n_queens(int now,int all){
+    for(int y=1;y<=all;y++){
+        if(now>all){
+            print();
+            break;
+        }
+        if(is_safe(now,y)){
+            location[now]=y;
+            n_queens(now+1,number);
+        }
+    }
+}
+
+bool is_safe(int x,int y){
+    int i=1;
+    for(i;i<=x-1;i++){
+        if(y == location[i] || abs(x - i) == abs(y - location[i]))return 0;
+    }
+    return 1;
+}
+
+int print(){
+    for(int i=1;i<=number;i++){
+        for(int j=1;j<=number;j++){
+            if(location[i]!=j){
+                putchar('o');
+            }else{
+                putchar('x');
+            }
+        }
+        putchar('\n');
+    }
+    count++;
+    putchar('\n');
+    return 0;
+}
+
+int main(){
+    scanf("%d",&number);
+    n_queens(1,number);
+    printf("Total solutions: %d\n", count);
+    return 0;
+}
